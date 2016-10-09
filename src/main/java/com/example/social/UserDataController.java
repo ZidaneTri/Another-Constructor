6464996cdc9595f2d69da860bfa3126befa7d59e
ZidaneTri <gavrilik.al@gmail.com>
2016-10-09 16:30:30 +0300

    some features

---
+++ src/main/java/com/example/social/UserDataController.java
@@ -0,0 +1,35 @@
+package com.example.social;
+
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/userdata")
+public class UserDataController {
+
+    private final ProviderSignInUtils signInUtils;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    public UserDataController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository connectionRepository) {
+        signInUtils = new ProviderSignInUtils(connectionFactoryLocator, connectionRepository);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public User sendData(Authentication principal) {
+        User user = userService.findByUserId((String) principal.getCredentials());
+        return user;
+    }
+}
